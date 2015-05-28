e16b254666dd433480e1a9c712981eeea3c7a80a
calvin-li <calli@ucdavis.edu>
2015-05-27 21:15:44 -0700

    trimmed g4 file

--- antlrsqlite/Main.java
+++ antlrsqlite/Main.java
@@ -1,5 +1,10 @@
+import java.io.*;
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.NullPointerException;
+import java.lang.System;
 import java.nio.charset.StandardCharsets;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
@@ -7,14 +12,25 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.misc.Interval;
 
-public class MutateQueries {
+public class Main {
     public static void main(String args[]) {
         System.out.println("Starting transformation...");
-        ByteArrayInputStream istr = new ByteArrayInputStream("SELECT * FROM tab WHERE id=3".getBytes(StandardCharsets.UTF_8));
+        //take input from file in command line, else use stdin
+        String inputFile = null;
+        if ( args.length>0 ) inputFile = args[0];
+        InputStream istr = System.in;
+        try {
+            istr = new FileInputStream(inputFile);
+        }catch (java.io.FileNotFoundException e){
+            System.err.println("File not found");
+        }catch (NullPointerException e){
+            System.err.println("Null pointer on input");
+        }
         ANTLRInputStream in = null;
 		try {
 			in = new ANTLRInputStream(istr);
 		} catch (IOException e) {
+            System.err.println("Input stream error");
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -25,6 +41,6 @@ public class MutateQueries {
         ParserRuleContext tree = parser.select_stmt();
         Interval before = new Interval(tree.start.getStartIndex(), tree.stop.getStopIndex());
         System.out.println(in.getText(before));
-        System.out.println(tree.toStringTree(parser));
+        //System.out.println(tree.toStringTree(parser));
     }
 }
