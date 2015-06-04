f650c1a68574328f3a88e627bf8450fa520ae010
Ben Mishkanian <benmishkanian@ucdavis.edu>
2015-06-04 12:31:32 -0700

    compilable intermediate commit

--- antlrsqlite/InvariantFinder.java
+++ antlrsqlite/InvariantFinder.java
@@ -1,5 +1,19 @@
 package antlrsqlite;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class InvariantFinder {
-
+	/**
+	 * Maps database table names to a list of invariants which reference 
+	 * columns in that table.
+	 */
+	private HashMap<String, ArrayList<String>> invariantTable;
+	
+	public InvariantFinder(String invariantFilePath) throws IOException {
+		// Initialize table of invariants
+		Files.newBufferedReader(Paths.get(invariantFilePath)).lines();
+	}
 }
