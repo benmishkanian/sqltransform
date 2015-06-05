8b96072ac666eb8aac95aa6eb55878c71215649d
calvin-li <calli@ucdavis.edu>
2015-06-04 17:47:08 -0700

    added functions to insert into query

--- antlrsqlite/Visitor.java
+++ antlrsqlite/Visitor.java
@@ -2,14 +2,18 @@ package antlrsqlite;
 import antlrsqlite.*;
 import antlrsqlite.SQLiteParser.*;
 
+import java.util.ArrayList;
+
 public class Visitor extends SQLiteBaseVisitor<Integer> {
 
 	String fullText;
+	ArrayList<Integer> hooks = new ArrayList<Integer>();
 
 	@Override
 	public Integer visitWhere_expr(Where_exprContext ctx) {
-		int startpos = ctx.start.getCharPositionInLine();
-		//System.out.println(fullText.substring(startpos, startpos+10));
+		int startPos = ctx.start.getCharPositionInLine();
+		addToHooks(startPos, "WHERE");
+		System.out.println(hooks.get(0));
 
 		super.visitWhere_expr(ctx);
 
@@ -18,11 +22,13 @@ public class Visitor extends SQLiteBaseVisitor<Integer> {
 
 	@Override
 	public Integer visitExpr(ExprContext ctx) {
-
-		System.out.println(ctx.getParent().getText());
 	
 		super.visitExpr(ctx);
 
 		return 0;
 	}//visitExpr
+	
+	void addToHooks(int index, String nextToken) {
+		hooks.add(index + nextToken.length() + 1);
+	}//addToHooks
 }
